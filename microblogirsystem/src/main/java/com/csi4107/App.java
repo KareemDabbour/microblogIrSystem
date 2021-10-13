31d748e198d6acb30c1649ad02ef97c3e31113cb
Kareem Dabbour <kareemdabbour@Kareems-MacBook-Pro.local>
2021-10-12 22:19:36 -0400

    making this a maven project

---
+++ microblogirsystem/src/main/java/com/csi4107/App.java
@@ -0,0 +1,15 @@
+package com.csi4107;
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
